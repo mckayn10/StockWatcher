bc7b6e1d2902ad14cf46ffea159271bbd92d02f6
McKay Nilsson <mckay.nilsson@gmail.com>
2020-03-19 19:54:36 -0600

    home view/header set

---
+++ src/Views/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.scss';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-container">
+                <h1>Stock Watcher</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
