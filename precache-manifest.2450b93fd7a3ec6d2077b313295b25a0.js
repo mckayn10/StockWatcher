77c3c3bea7666167a46d7ddfcc00d82902d15300
mckaynilsson10 <nilsson@foreup.com>
2021-07-07 11:11:41 -0600

    Updates

---
+++ precache-manifest.2450b93fd7a3ec6d2077b313295b25a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ce1299ce9095f292c57b3b944aef33",
+    "url": "/StockWatcher/index.html"
+  },
+  {
+    "revision": "8ad2300d12688e41c70c",
+    "url": "/StockWatcher/static/css/main.9bd7f25c.chunk.css"
+  },
+  {
+    "revision": "c0171d3df74d482dcd35",
+    "url": "/StockWatcher/static/js/2.5f64891d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/StockWatcher/static/js/2.5f64891d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad2300d12688e41c70c",
+    "url": "/StockWatcher/static/js/main.ee5f5a5d.chunk.js"
+  },
+  {
+    "revision": "e8c79c4009685597bb26",
+    "url": "/StockWatcher/static/js/runtime-main.2176e8da.js"
+  }
+]);
